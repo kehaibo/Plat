f2c61d685b1729517d07f51aea86d8740493b5b1
lenovo <949096279@qq.com>
2018-12-11 16:16:44 +0800

    'remove'

--- devicecnt/app/config.py
+++
@@ -1,6 +0,0 @@
-import os 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-signup_email_html=os.path.join(BASE_DIR,'app\\templates\\signup_email.html')
--- devicecnt/mysqlstart.bat
+++
@@ -1,5 +0,0 @@
-@echo off 
-e:
-cd \mysql-5.7.20-winx64\bin
-mysqld.exe
-echo 'mysql start...'
